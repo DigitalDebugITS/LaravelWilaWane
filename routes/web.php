76c363652adfab49c5429416e710dcc73574c28f
Ernest Lubinda <ernestlubinda1@gmail.com>
2024-06-26 03:33:54 +0200

    fdg

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ResourcesController;
-
-Route::get('/', [BlogController::class, 'index']);
-Route::get('/resources', [ResourcesController::class, 'index'])->name('resources.index');
